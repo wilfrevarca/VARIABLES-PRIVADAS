06f5132ca5648e3b8ab5ec0d80343cfc0499c495
wilfredo <109392939+wilfrevarca@users.noreply.github.com>
2022-08-03 17:18:14 -0500

    Add files via upload

---
+++ privadad/privadad/src/privadad/Main.java
@@ -0,0 +1,25 @@
+ package privadad;
+
+    public class Main {
+
+
+        public static void main(String[] args) {
+
+            class persona {
+            }
+
+
+        }
+
+        private static void edad() {
+
+        }
+
+        private static void nombre() {
+
+        }
+       private static void telefono() {
+
+            System.out.println("edad,nombre,telefono");
+       }
+       }
